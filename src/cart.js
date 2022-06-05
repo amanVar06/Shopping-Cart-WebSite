5debd6d07c604ad18fa67bb5a8263161bf60221d
amanVar06 <98205162+amanVar06@users.noreply.github.com>
2022-06-05 13:26:09 +0530

    Add files via upload

--- src/cart.js
+++ src/cart.js
@@ -1,131 +1,149 @@
 let label = document.getElementById("label");
-let ShoppingCart = document.getElementById("shopping-cart");
+let shoppingCart = document.getElementById("shopping-cart");
 
-let basket = JSON.parse(localStorage.getItem("data")) || [];
+//console.log(shopItemsData);
+let basket =  JSON.parse(localStorage.getItem("Data")) || [];
 
-let calculation = () => {
-  let cartIcon = document.getElementById("cartAmount");
-  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+
+let calculation = ()=> {
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x,y)=>x+y,0);
 };
 
 calculation();
 
+//ES6 arrow function
 let generateCartItems = () => {
-  if (basket.length !== 0) {
-    return (ShoppingCart.innerHTML = basket
-      .map((x) => {
-        let { id, item } = x;
-        let search = shopItemsData.find((y) => y.id === id) || [];
-        return `
-      <div class="cart-item">
-        <img width="100" src=${search.img} alt="" />
-        <div class="details">
+    if(basket.length !== 0) {
+        //this will run for all the elements/items in the basket
+        //x will target all the data from the local storage one by one then run the function
+        return (shoppingCart.innerHTML = basket.map((x) => {
+            //console.log(x);
+            let {id,item} = x;
+            let search = shopItemsData.find((y)=>y.id === id) || [];
+            let {img, name, price} = search;//object destructure
+            return `
+            <div class="cart-item">
+                <img width = "100" src ="${img}" alt = ""/> 
+                <div class= "details">
+                    <div class= "title-price-x">
+                        <h4 class = "title-price">
+                            <p>${name}</p>
+                            <p class = "cart-item-price">$ ${price}</p>
+                        </h4>
+                        <i onclick = "removeItem(${id})" class="bi bi-x"></i>
+                    </div>
 
-          <div class="title-price-x">
-              <h4 class="title-price">
-                <p>${search.name}</p>
-                <p class="cart-item-price">$ ${search.price}</p>
-              </h4>
-              <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
-          </div>
-
-          <div class="buttons">
-              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-              <div id=${id} class="quantity">${item}</div>
-              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
-          </div>
-
-          <h3>$ ${item * search.price}</h3>
-        </div>
-      </div>
-      `;
-      })
-      .join(""));
-  } else {
-    ShoppingCart.innerHTML = ``;
-    label.innerHTML = `
-    <h2>Cart is Empty</h2>
-    <a href="index.html">
-      <button class="HomeBtn">Back to home</button>
-    </a>
-    `;
-  }
+                    <div class="buttons">
+                        <i onclick = "decrement(${id})" class="bi bi-dash"></i>
+                        <div id=${id} class="quantity">${item}</div>
+                        <i onclick = "increment(${id})" class="bi bi-plus"></i>    
+                    </div>
+                    
+                    <h3>$ ${item * price}</h3>
+                </div>
+            </div>              
+            `;
+        }).join(""));//to remove comma use .join("");
+    }else{
+        shoppingCart.innerHTML = ``;
+        label.innerHTML = `
+        <h2>Cart is Empty</h2>
+        <a href = "index.html">
+            <button class = "HomeBtn">Back to Home</button>
+        </a>  
+        `;
+    }
 };
 
 generateCartItems();
 
 let increment = (id) => {
-  let selectedItem = id;
-  let search = basket.find((x) => x.id === selectedItem.id);
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
 
-  if (search === undefined) {
-    basket.push({
-      id: selectedItem.id,
-      item: 1,
-    });
-  } else {
-    search.item += 1;
-  }
+    if(search === undefined){
+        basket.push({
+            id: selectedItem.id,
+            item: 1
+        });
+    }else{
+        search.item += 1;
+    }
+    //storing in the local storage
+    localStorage.setItem("Data",JSON.stringify(basket));
 
-  generateCartItems();
-  update(selectedItem.id);
-  localStorage.setItem("data", JSON.stringify(basket));
+    generateCartItems();// re-rendering of our cart
+    update(selectedItem.id);
+
 };
-let decrement = (id) => {
-  let selectedItem = id;
-  let search = basket.find((x) => x.id === selectedItem.id);
 
-  if (search === undefined) return;
-  else if (search.item === 0) return;
-  else {
-    search.item -= 1;
-  }
-  update(selectedItem.id);
-  basket = basket.filter((x) => x.item !== 0);
-  generateCartItems();
-  localStorage.setItem("data", JSON.stringify(basket));
+let decrement = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+
+    if(search === undefined){
+        return;
+    }else if(search.item === 0){
+        return;
+    }
+    else{
+        search.item -= 1;
+    }
+    
+
+    update(selectedItem.id);
+    basket = basket.filter((x)=> x.item !== 0);
+
+    generateCartItems();// re-rendering of our cart
+    
+    localStorage.setItem("Data",JSON.stringify(basket));//put this line at the bottom when all the updates are done to see changes in the local storage
+
 };
 
 let update = (id) => {
-  let search = basket.find((x) => x.id === id);
-  // console.log(search.item);
-  document.getElementById(id).innerHTML = search.item;
-  calculation();
-  TotalAmount();
+    let search = basket.find((x) => x.id === id);
+    //console.log(search.item);
+
+    document.getElementById(id).innerHTML = search.item;
+    calculation();
+    totalAmount();
 };
+
 
 let removeItem = (id) => {
-  let selectedItem = id;
-  // console.log(selectedItem.id);
-  basket = basket.filter((x) => x.id !== selectedItem.id);
-  generateCartItems();
-  TotalAmount();
-  localStorage.setItem("data", JSON.stringify(basket));
-};
+    let selectedItem = id;
+    //console.log(selectedItem.id);//it will print entire HTML element but why?
+    basket = basket.filter((x) => x.id !== selectedItem.id);
+    generateCartItems();
+    calculation();//my logic to update cart amount in red
+    totalAmount();//to update final amount after cross
+
+    localStorage.setItem("Data",JSON.stringify(basket));
+}
 
 let clearCart = () => {
-  basket = [];
-  generateCartItems();
-  localStorage.setItem("data", JSON.stringify(basket));
-};
+    basket = [];
+    generateCartItems();
+    calculation();//to update cart amount
+    localStorage.setItem("Data",JSON.stringify(basket));
+}
 
-let TotalAmount = () => {
-  if (basket.length !== 0) {
-    let amount = basket
-      .map((x) => {
-        let { item, id } = x;
-        let search = shopItemsData.find((y) => y.id === id) || [];
+let totalAmount = () => {
+    if (basket.length !== 0) {
+        let amount = basket.map((x) => {
+            let {id,item} = x;
+            let search = shopItemsData.find((y)=>y.id === id) || [];
 
-        return item * search.price;
-      })
-      .reduce((x, y) => x + y, 0);
-    // console.log(amount);
-    label.innerHTML = `
-    <h2>Total Bill : $ ${amount}</h2>
-    <button class="checkout">Checkout</button>
-    <button onclick="clearCart()" class="removeAll">Clear Cart</button>
-    `;
-  } else return;
-};
+            return item * search.price;
+        }).reduce((x,y) => x+y,0);
+        //console.log(amount);
+        label.innerHTML = `
+            <h2>Total Bill : $ ${amount}</h2>
+            <button class = "checkOut">Checkout</button>
+            <button onclick = "clearCart()" class = "removeAll">Clear Cart</button>
+        `;
+    }else return;
+}
 
-TotalAmount();
+totalAmount();
--- src/main.js
+++ src/main.js
@@ -1,78 +1,109 @@
 let shop = document.getElementById("shop");
+//console.log(shop);
 
-let basket = JSON.parse(localStorage.getItem("data")) || [];
+let basket =  JSON.parse(localStorage.getItem("Data")) || [];
 
 let generateShop = () => {
-  return (shop.innerHTML = shopItemsData
-    .map((x) => {
-      let { id, name, price, desc, img } = x;
-      let search = basket.find((x) => x.id === id) || [];
-      return `
-    <div id=product-id-${id} class="item">
-        <img width="220" src=${img} alt="">
-        <div class="details">
-          <h3>${name}</h3>
-          <p>${desc}</p>
-          <div class="price-quantity">
-            <h2>$ ${price} </h2>
-            <div class="buttons">
-              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-              <div id=${id} class="quantity">
-              ${search.item === undefined ? 0 : search.item}
-              </div>
-              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
-            </div>
-          </div>
-        </div>
-      </div>
-    `;
-    })
-    .join(""));
+    return (shop.innerHTML = shopItemsData.map((x)=>{ 
+        //x means running map for all the items
+        //destructuring the object //to prevent writing x. multiple times
+        let {id, name, price, desc, img} = x;
+        let search1 = basket.find((x) => x.id === id) || [];
+        return  `
+        <div id = product-id-${id} class="item">
+                <img width = "220" src="${img}" alt="">
+                <div class="details">
+                    <h3>${name}</h3>
+                    <p>${desc}</p>
+                    <div class="price_quantity">
+                        <h2>$ ${price}</h2>
+                        <div class="buttons">
+                            <i onclick = "decrement(${id})" class="bi bi-dash"></i>
+                            <div id=${id} class="quantity">
+                            ${search1.item === undefined? 0 : search1.item}
+                            </div>
+                            <i onclick = "increment(${id})" class="bi bi-plus"></i>    
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+    }).join(""));
 };
 
 generateShop();
 
+//we gave id to uniquely identify each item
+
+/**
+* ? i didn't get how selecteitem.id gets the id and why the thing is printing when i am try to printing selectedItem
+**/
+
 let increment = (id) => {
-  let selectedItem = id;
-  let search = basket.find((x) => x.id === selectedItem.id);
+    let selectedItem = id;
+    //console.log(selectedItem);//it will print entire HTML element but why?
+    let search = basket.find((x) => x.id === selectedItem.id);
 
-  if (search === undefined) {
-    basket.push({
-      id: selectedItem.id,
-      item: 1,
-    });
-  } else {
-    search.item += 1;
-  }
+    if(search === undefined){
+        basket.push({
+            id: selectedItem.id,
+            item: 1
+        });
+    }else{
+        search.item += 1;
+    }
+    //storing in the local storage
+    localStorage.setItem("Data",JSON.stringify(basket));
 
-  // console.log(basket);
-  update(selectedItem.id);
-  localStorage.setItem("data", JSON.stringify(basket));
+    //console.log(basket);
+    update(selectedItem.id);
+
 };
+
 let decrement = (id) => {
-  let selectedItem = id;
-  let search = basket.find((x) => x.id === selectedItem.id);
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
 
-  if (search === undefined) return;
-  else if (search.item === 0) return;
-  else {
-    search.item -= 1;
-  }
-  update(selectedItem.id);
-  basket = basket.filter((x) => x.item !== 0);
-  // console.log(basket);
-  localStorage.setItem("data", JSON.stringify(basket));
+    if(search === undefined){
+        return;
+    }else if(search.item === 0){
+        return;
+    }
+    else{
+        search.item -= 1;
+    }
+    
+
+    update(selectedItem.id);
+    basket = basket.filter((x)=> x.item !== 0);
+
+    //console.log(basket);
+    
+    localStorage.setItem("Data",JSON.stringify(basket));//put this line at the bottom when all the updates are done to see changes in the local storage
+
 };
+
+//ES6 arrow function
+
+let calculation = ()=> {
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x,y)=>x+y,0);
+    //0 means its a default number 
+    //we want the calcultion to  start from 0
+    //using map function
+    //two arguments x and y one is previous number and another is next number
+};
+
 let update = (id) => {
-  let search = basket.find((x) => x.id === id);
-  // console.log(search.item);
-  document.getElementById(id).innerHTML = search.item;
-  calculation();
-};
+    let search = basket.find((x) => x.id === id);
+    //console.log(search.item);
 
-let calculation = () => {
-  let cartIcon = document.getElementById("cartAmount");
-  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+    document.getElementById(id).innerHTML = search.item;
+    calculation();
 };
 
 calculation();
+
+//use local storage to preserve the data even after referesh
+//built in storage of our browser
+//i want specifically basket to store in the local storage
+//need to retrieve the data also from the local storage after refresh
